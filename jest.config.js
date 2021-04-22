f6dc018ab0f3e7a11780d109f87922578d0b65ed
Mike Hardy <github@mikehardy.net>
2021-04-22 18:25:51 -0500

    Initial commit
    (mostly grabbed from react-native-firebase, bent to fit admob post-extraction)

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  maxConcurrency: 10,
+  preset: './tests/node_modules/react-native-web/jest-preset.js',
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFiles: ['./jest.setup.ts'],
+  testMatch: [
+    '__tests__/**/*.test.(ts|js)',
+  ],
+  modulePaths: ['node_modules'],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+};
