2b0c0174a7cc57419a692c84c599fed550565659
Mike Hardy <github@mikehardy.net>
2021-08-31 21:32:46 -0500

    test: yarn tests:jest and yarn tsc:compile are working now
    main extraction of javascript from react-native-firebase appears to work,
    native layer is next

---
+++ __tests__/rewarded.test.tsx
@@ -0,0 +1,4 @@
+describe('Admob Rewarded', () => {
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('skip', () => {});
+});
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   maxConcurrency: 10,
-  preset: './tests/node_modules/react-native-web/jest-preset.js',
+  preset: './node_modules/react-native/jest-preset.js',
   transform: {
     '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
     '\\.(ts|tsx)$': 'ts-jest',
   },
   setupFiles: ['./jest.setup.ts'],
-  testMatch: ['__tests__/**/*.test.(ts|js)'],
+  testRegex: '(/^__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   modulePaths: ['node_modules'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
