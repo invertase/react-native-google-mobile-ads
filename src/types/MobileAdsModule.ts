d147da1078c3d6febd0391edc4372e1756ca8f61
Dylan Companjen <dylancompanjen@gmail.com>
2021-12-19 11:18:03 +0100

    add enum InitializationState

--- src/types/MobileAdsModule.ts
+++ src/types/MobileAdsModule.ts
@@ -1,12 +1,24 @@
 import { RequestConfiguration } from './RequestConfiguration';
 
+export enum InitializationState {
+  /**
+   * The mediation adapter is less likely to fill ad requests.
+   */
+  AdapterInitializationStateNotReady = 0,
+
+  /**
+   * The mediation adapter is ready to service ad requests.
+   */
+  AdapterInitializationStateReady = 1,
+}
+
 /**
  * An immutable snapshot of a mediation adapter's initialization status.
  */
 export type AdapterStatus = {
   name: string;
   description: string;
-  status: 0 | 1;
+  status: InitializationState;
 };
 
 /**
