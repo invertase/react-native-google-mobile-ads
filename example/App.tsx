118525725c8aff37d80eee07e069ed8d720c717a
Mike Hardy <github@mikehardy.net>
2022-01-25 15:48:42 -0500

    test(example): add Banner, Interstitial, Rewarded tests
    they are manual but they work! 100% on Android, and 2/3 on iOS,
    for some reason the rewarded ad is no-fill erroring even though it
    uses the official google ios rewarded ad test id

--- example/App.tsx
+++ example/App.tsx
@@ -1,29 +1,126 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Button, SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import {Test, TestRegistry, TestResult, TestRunner, TestType} from 'jet';
+
 import {
-  AutoExecutableTest,
-  TestRegistry,
-  TestResult,
-  TestRunner,
-  TestType,
-} from 'jet';
+  InterstitialAd,
+  TestIds,
+  BannerAd,
+  BannerAdSize,
+  RewardedAd,
+} from 'react-native-google-mobile-ads';
+
+const interstitialAdUnitId = TestIds.INTERSTITIAL;
+
+const interstitial = InterstitialAd.createForAdRequest(interstitialAdUnitId, {
+  // requestNonPersonalizedAdsOnly: true,
+  // keywords: ['fashion', 'clothing'],
+});
 
 // To implement a test you must make a new object implementing a specific interface.
-class ExampleTest implements AutoExecutableTest {
+class InterstitialTest implements Test {
+  constructor() {
+    interstitial.load();
+  }
+
   getPath(): string {
-    return 'Example';
+    return 'Interstitial';
   }
 
   getTestType(): TestType {
-    return TestType.AutoExecutable;
+    return TestType.Interactive;
   }
 
   render(onMount: (component: any) => void): React.ReactNode {
     return (
-      <View ref={onMount}>
-        <Text>
-          {'This is rendered if a user taps the test in the test list.'}
-        </Text>
+      <View style={styles.testSpacing} ref={onMount}>
+        <Button
+          title="Show Interstitial"
+          onPress={() => {
+            interstitial.show();
+          }}
+        />
+      </View>
+    );
+  }
+
+  execute(component: any, complete: (result: TestResult) => void): void {
+    let results = new TestResult();
+    try {
+      // You can do anything here, it will execute on-device + in-app. Results are aggregated + visible in-app.
+    } catch (error) {
+      results.errors.push('Received unexpected error...');
+    } finally {
+      complete(results);
+    }
+  }
+}
+
+const bannerAdUnitId = TestIds.BANNER;
+
+class BannerTest implements Test {
+  getPath(): string {
+    return 'Banner';
+  }
+
+  getTestType(): TestType {
+    return TestType.Interactive;
+  }
+
+  render(onMount: (component: any) => void): React.ReactNode {
+    return (
+      <View style={styles.testSpacing} ref={onMount}>
+        <BannerAd
+          unitId={bannerAdUnitId}
+          size={BannerAdSize.FULL_BANNER}
+          requestOptions={{
+            requestNonPersonalizedAdsOnly: true,
+          }}
+        />
+      </View>
+    );
+  }
+
+  execute(component: any, complete: (result: TestResult) => void): void {
+    let results = new TestResult();
+    try {
+      // You can do anything here, it will execute on-device + in-app. Results are aggregated + visible in-app.
+    } catch (error) {
+      results.errors.push('Received unexpected error...');
+    } finally {
+      complete(results);
+    }
+  }
+}
+
+const rewardedAdUnitId = TestIds.REWARDED;
+
+const rewarded = RewardedAd.createForAdRequest(rewardedAdUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
+class RewaredTest implements Test {
+  constructor() {
+    rewarded.load();
+  }
+
+  getPath(): string {
+    return 'Rewarded';
+  }
+
+  getTestType(): TestType {
+    return TestType.Interactive;
+  }
+
+  render(onMount: (component: any) => void): React.ReactNode {
+    return (
+      <View style={styles.testSpacing} ref={onMount}>
+        <Button
+          title="Show Rewarded"
+          onPress={() => {
+            rewarded.show();
+          }}
+        />
       </View>
     );
   }
@@ -41,7 +138,9 @@ class ExampleTest implements AutoExecutableTest {
 }
 
 // All tests must be registered - a future feature will allow auto-bundling of tests via configured path or regex
-TestRegistry.registerTest(new ExampleTest());
+TestRegistry.registerTest(new BannerTest());
+TestRegistry.registerTest(new InterstitialTest());
+TestRegistry.registerTest(new RewaredTest());
 
 const App = () => {
   return (
@@ -52,5 +151,12 @@ const App = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  testSpacing: {
+    margin: 10,
+    padding: 10,
+  },
+});
 
 export default App;
