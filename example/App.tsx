7dcf5df0c7a3de420789ba584ba1728d79dc11cf
Birger Naß <n.b@posteo.de>
2022-03-02 22:38:14 +0100

    test(example): add AdConsent test

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {Button, SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {Test, TestRegistry, TestResult, TestRunner, TestType} from 'jet';
 
 import {
+  AdsConsent,
+  AdsConsentDebugGeography,
+  AdsConsentStatus,
   AppOpenAd,
   InterstitialAd,
   TestIds,
@@ -182,11 +192,64 @@ class RewaredTest implements Test {
   }
 }
 
+class AdConsentTest implements Test {
+  getPath(): string {
+    return 'Consent Form';
+  }
+
+  getTestType(): TestType {
+    return TestType.Interactive;
+  }
+
+  render(onMount: (component: any) => void): React.ReactNode {
+    return (
+      <View style={styles.testSpacing} ref={onMount}>
+        <Button
+          title="Show Consent Form"
+          onPress={async () => {
+            const consentInfo = await AdsConsent.requestInfoUpdate({
+              debugGeography: AdsConsentDebugGeography.EEA,
+              testDeviceIdentifiers: [],
+            });
+
+            if (
+              consentInfo.isConsentFormAvailable &&
+              consentInfo.status === AdsConsentStatus.REQUIRED
+            ) {
+              await AdsConsent.showForm();
+            }
+          }}
+        />
+
+        <Text>
+          This test case will not work with the test App ID. You must configure
+          your real App ID in app.json and the Consent Form in AdMob/Ad Manager.
+          If you are running this test on a device instead of an emulator and if
+          you are currently not located in EEA, you have to add your Decive ID
+          to the testDeviceIdentifiers of this test case as well.
+        </Text>
+      </View>
+    );
+  }
+
+  execute(component: any, complete: (result: TestResult) => void): void {
+    let results = new TestResult();
+    try {
+      // You can do anything here, it will execute on-device + in-app. Results are aggregated + visible in-app.
+    } catch (error) {
+      results.errors.push('Received unexpected error...');
+    } finally {
+      complete(results);
+    }
+  }
+}
+
 // All tests must be registered - a future feature will allow auto-bundling of tests via configured path or regex
 TestRegistry.registerTest(new BannerTest());
 TestRegistry.registerTest(new AppOpenTest());
 TestRegistry.registerTest(new InterstitialTest());
 TestRegistry.registerTest(new RewaredTest());
+TestRegistry.registerTest(new AdConsentTest());
 
 const App = () => {
   return (
