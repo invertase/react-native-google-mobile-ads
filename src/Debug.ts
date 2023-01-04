85a0c3e00407d0a74fc1f35d71bda1e72725b30c
Aidan Host <aidan.host@media24.com>
2023-01-04 10:33:23 +0200

    chore(debug, ts): add debug interface

--- src/Debug.ts
+++ src/Debug.ts
@@ -1,8 +1,9 @@
 import { NativeModules } from 'react-native';
+import { DebugInterface } from './types/Debug.interface';
 
 const RNGoogleMobileAdsDebugModule = NativeModules.RNGoogleMobileAdsDebugModule;
 
-export const Debug = {
+export const Debug: DebugInterface = {
   openDebugMenu(adUnit: string) {
     RNGoogleMobileAdsDebugModule.openDebugMenu(adUnit);
   },
---
+++ src/types/Debug.interface.ts
@@ -0,0 +1,3 @@
+export interface DebugInterface {
+    openDebugMenu: (adUnit:string)=>void
+}
