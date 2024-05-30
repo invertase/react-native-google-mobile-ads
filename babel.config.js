0f7ae5b7d6fabfcfb1b02110d6dcb77a76367f60
Dylan <dylancompanjen@gmail.com>
2024-05-30 09:18:44 +0200

    build(deps): switch from @react-native/babel-preset to metro-react-native-babel-preset

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
 module.exports = {
   env: {
     test: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 'current',
-            },
-          },
-        ],
-        'module:@react-native/babel-preset',
-      ],
+      presets: ['module:metro-react-native-babel-preset'],
     },
   },
   plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]],
