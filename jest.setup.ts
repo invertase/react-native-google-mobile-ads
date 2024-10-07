8866d8a349be8169588ad9834fb501a31047ced6
Jonathan Ehwald <github@ehwald.info>
2024-10-07 20:03:16 +0200

    fix: jest setup was voiding custom turbo mocks (#636)
    * fix: jest setup was voiding custom turbo mocks
    * fix: properly scope test-specific mocks
    * docs: document jest mocking on a dedicated page

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,10 +3,6 @@ import * as ReactNative from 'react-native';
 jest.doMock('react-native', () => {
   return Object.setPrototypeOf(
     {
-      Platform: {
-        OS: 'android',
-        select: () => {},
-      },
       NativeModules: {
         ...ReactNative.NativeModules,
         RNAppModule: {
@@ -28,6 +24,7 @@ jest.doMock('react-native', () => {
         RNGoogleMobileAdsConsentModule: {},
       },
       TurboModuleRegistry: {
+        ...ReactNative.TurboModuleRegistry,
         getEnforcing: () => {
           return {
             initialize: jest.fn(),
