17adfa8c8823a5c3ac15d784fb76e9b2f28afcd2
MyungJoo Lee <audwn0326@gmail.com>
2025-08-21 11:06:20 +0900

    fix(native ads, ios): wrong native ad prop type

--- ios/RNGoogleMobileAds/RNGoogleMobileAdsNativeView.mm
+++ ios/RNGoogleMobileAds/RNGoogleMobileAdsNativeView.mm
@@ -52,7 +52,7 @@ using namespace facebook::react;
 
 - (instancetype)initWithFrame:(CGRect)frame {
   if (self = [super initWithFrame:frame]) {
-    static const auto defaultProps = std::make_shared<const RNGoogleMobileAdsBannerViewProps>();
+    static const auto defaultProps = std::make_shared<const RNGoogleMobileAdsNativeViewProps>();
     _props = defaultProps;
 
     _bridge = [RCTBridge currentBridge];
