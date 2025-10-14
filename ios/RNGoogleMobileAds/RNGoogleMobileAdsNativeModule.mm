e5c12148fda79dc0dde060f5f50de99f910470db
Kim Kwang Hyun <calrosban@gmail.com>
2025-10-14 20:16:38 +0900

    fix(native ad, ios): fixed ios crash

--- ios/RNGoogleMobileAds/RNGoogleMobileAdsNativeModule.mm
+++ ios/RNGoogleMobileAds/RNGoogleMobileAdsNativeModule.mm
@@ -95,9 +95,9 @@ RCT_EXPORT_METHOD(
           @"price" : nativeAd.price ?: [NSNull null],
           @"store" : nativeAd.store ?: [NSNull null],
           @"starRating" : nativeAd.starRating ?: [NSNull null],
-          @"icon" : nativeAd.icon != nil
-              ? @{@"scale" : @(nativeAd.icon.scale), @"url" : nativeAd.icon.imageURL.absoluteString}
-              : [NSNull null],
+          @"icon" : (nativeAd.icon && nativeAd.icon.imageURL != nil)
+            ? @{@"scale": @(nativeAd.icon.scale), @"url": nativeAd.icon.imageURL.absoluteString}
+            : [NSNull null],
           @"mediaContent" : @{
             @"aspectRatio" : @(nativeAd.mediaContent.aspectRatio),
             @"hasVideoContent" : @(nativeAd.mediaContent.hasVideoContent),
