84b1ac3178d26943f517a163c16ff4580320f17a
Mike Hardy <github@mikehardy.net>
2025-10-22 18:25:17 -0500

    test(jest): fixup jest tests for modern jest / refresh snapshot

--- __tests__/consent.test.ts
+++ __tests__/consent.test.ts
@@ -4,28 +4,28 @@ describe('Google Mobile Ads AdsConsent', function () {
   describe('requestInfoUpdate', function () {
     it('throws if options are not an object', function () {
       // @ts-ignore
-      expect(() => AdsConsent.requestInfoUpdate('123')).toThrowError(
+      expect(() => AdsConsent.requestInfoUpdate('123')).toThrow(
         "AdsConsent.requestInfoUpdate(*) 'options' expected an object value.",
       );
     });
 
     it('throws if options.debugGeography is not a valid value.', function () {
       // @ts-ignore
-      expect(() => AdsConsent.requestInfoUpdate({ debugGeography: -1 })).toThrowError(
+      expect(() => AdsConsent.requestInfoUpdate({ debugGeography: -1 })).toThrow(
         "AdsConsent.requestInfoUpdate(*) 'options.debugGeography' expected one of AdsConsentDebugGeography.DISABLED, AdsConsentDebugGeography.EEA, AdsConsentDebugGeography.NOT_EEA, AdsConsentDebugGeography.REGULATED_US_STATE or AdsConsentDebugGeography.OTHER.",
       );
     });
 
     it('throws if options.tagForUnderAgeOfConsent is not a boolean.', function () {
       // @ts-ignore
-      expect(() => AdsConsent.requestInfoUpdate({ tagForUnderAgeOfConsent: '123' })).toThrowError(
+      expect(() => AdsConsent.requestInfoUpdate({ tagForUnderAgeOfConsent: '123' })).toThrow(
         "AdsConsent.requestInfoUpdate(*) 'options.tagForUnderAgeOfConsent' expected a boolean value.",
       );
     });
 
     it('throws if options.testDeviceIdentifiers is not an array', function () {
       // @ts-ignore
-      expect(() => AdsConsent.requestInfoUpdate({ testDeviceIdentifiers: '123' })).toThrowError(
+      expect(() => AdsConsent.requestInfoUpdate({ testDeviceIdentifiers: '123' })).toThrow(
         "AdsConsent.requestInfoUpdate(*) 'options.testDeviceIdentifiers' expected an array of string values.",
       );
     });
