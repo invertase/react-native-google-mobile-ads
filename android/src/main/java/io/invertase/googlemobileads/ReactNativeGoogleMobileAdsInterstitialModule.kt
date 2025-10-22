6c91d0181186073e876fef4c616c3f35731c6986
demon-sword <akkisfavourite@gmail.com>
2025-10-22 16:46:08 +0530

    fix: type casting added

--- android/src/main/java/io/invertase/googlemobileads/ReactNativeGoogleMobileAdsInterstitialModule.kt
+++ android/src/main/java/io/invertase/googlemobileads/ReactNativeGoogleMobileAdsInterstitialModule.kt
@@ -1,5 +1,6 @@
 package io.invertase.googlemobileads
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
@@ -11,19 +12,19 @@ class ReactNativeGoogleMobileAdsInterstitialModule(reactContext: ReactApplicatio
   private val instance: ReactNativeGoogleMobileAdsInterstitialModuleImpl = ReactNativeGoogleMobileAdsInterstitialModuleImpl(reactContext)
   override fun interstitialLoad(
     requestId: Double,
-    adUnitId: String?,
+    adUnitId: String,
     requestOptions: ReadableMap?
   ) {
-//    instance.interstitialLoad()
+    instance.interstitialLoad(requestId.toInt(), adUnitId, requestOptions?: Arguments.createMap())
   }
 
   override fun interstitialShow(
     requestId: Double,
-    adUnitId: String?,
+    adUnitId: String,
     showOptions: ReadableMap?,
-    promise: Promise?
+    promise: Promise
   ) {
-//    instance.interstitialShow()
+    instance.interstitialShow(requestId.toInt(), adUnitId, showOptions?: Arguments.createMap(),promise)
   }
   companion object {
     const val NAME = NativeInterstitialModuleSpec.NAME
