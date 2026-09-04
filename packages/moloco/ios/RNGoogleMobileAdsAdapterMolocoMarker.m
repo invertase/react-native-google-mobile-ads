17f474c2cc5bf6b74b1ae71bdd0eadcc31e843a4
Mike Hardy <github@mikehardy.net>
2026-09-04 10:28:46 -0500

    feat(adapters): add moloco GAM mediation package

---
+++ packages/moloco/ios/RNGoogleMobileAdsAdapterMolocoMarker.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+/**
+ * Package marker so the podspec has a compilable source file.
+ * Mediation is linked via the GoogleMobileAdsMediationMoloco pod dependency.
+ */
+@interface RNGoogleMobileAdsAdapterMolocoMarker : NSObject
+@end
+
+@implementation RNGoogleMobileAdsAdapterMolocoMarker
+@end
